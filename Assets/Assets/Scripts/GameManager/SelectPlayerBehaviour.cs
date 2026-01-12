fbf0ceaacef01d6522f29d79a0b8b8613286a57c
Robi-1224 <230087@student.glu.nl>
2026-01-12 01:49:34 +0100

    Character picker updated

--- Assets/Assets/Scripts/GameManager/SelectPlayerBehaviour.cs
+++ Assets/Assets/Scripts/GameManager/SelectPlayerBehaviour.cs
@@ -3,17 +3,39 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using UnityEngine.iOS;
 
 public class SelectPlayerBehaviour : MonoBehaviour
 {
+    [Header("Components")]
     [SerializeField] PlayerInput _player1Input;
     [SerializeField] PlayerInput _player2Input;
+    [SerializeField] GameObject _characterSelectPanel;
     private Button _playerButton;
-    public List<Gamepad> _gamepad;
+
+    [Header("Variables")]
+    private bool _player1Slotted = false;
+    private bool _player2Slotted = false;
+
 
     private void Awake()
     {
+        if (!Gamepad.current.enabled || !Keyboard.current.enabled)
+        {
+            InputSystem.EnableDevice(Gamepad.current);
+            InputSystem.EnableDevice(Keyboard.current);
+        }
         _playerButton = GetComponent<Button>();
+    }
+    private void PlayersSlottedBehaviour()
+    {
+        if (_player2Slotted && _player1Slotted)
+        {
+            Debug.Log("All Slotted");
+            _characterSelectPanel.SetActive(false);
+            InputSystem.EnableDevice(Gamepad.current);
+            InputSystem.EnableDevice(Keyboard.current);
+        }
     }
     public void OnPlayerSelect()
     {
@@ -21,16 +43,16 @@ public class SelectPlayerBehaviour : MonoBehaviour
         {
             switch(_playerButton.name)
             {
-                case "Molly": SwitchToGamepad(_player1Input); break;
-                case "Henry": SwitchToGamepad(_player2Input); break;
+                case "Molly": SwitchToGamepad(_player1Input); InputSystem.DisableDevice(Gamepad.current);_player1Slotted = true; PlayersSlottedBehaviour(); break;
+                case "Henry": SwitchToGamepad(_player2Input); InputSystem.DisableDevice(Gamepad.current); _player2Slotted = true; PlayersSlottedBehaviour(); break; 
             }
         }
         else if (Keyboard.current != null && Keyboard.current.enterKey.wasPressedThisFrame)
         {
             switch (_playerButton.name)
             {
-                case "Molly": SwitchToKeyboard(_player1Input); break;
-                case "Henry": SwitchToKeyboard(_player2Input); break;
+                case "Molly": SwitchToKeyboard(_player1Input); InputSystem.DisableDevice(Keyboard.current);_player1Slotted = true; PlayersSlottedBehaviour(); break;
+                case "Henry": SwitchToKeyboard(_player2Input); InputSystem.DisableDevice(Keyboard.current); _player2Slotted = true; PlayersSlottedBehaviour(); break;
             }
         }
     }
